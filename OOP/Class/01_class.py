b7d5609250329c63233f97e569c25153466e1488
Rudrajad2005 <rudrajad2005@gmail.com>
2024-08-15 11:05:42 +0530

    Updated

--- OOP/Class/01_class.py
+++ OOP/Class/01_class.py
@@ -1,9 +1,10 @@
 # Example 1 for understanding class and init method
 class Dog:
 
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+    def __init__(self, name, age):# -----> This is called an init method. This method has two leading underscores and two trailing underscores.
+        # ---> We defined init method to have three parameters: self, name, age. The self paramater is required in init method, and it must come first, before the other parameters 
+        self.name = name # -----> The two variables defined in the body  of the init method each have the prefix self. Any variable prefixed with self is available to every method in the class, and we'll also be able to access these variables through any instances created from the class.
+        self.age = age  
 
     def sit(self):
         print(f"\n{self.name} is now sitting.")
@@ -40,6 +41,27 @@ print(f"My dog's name is {my_dog.name}")
 print(f"My dog's age is {my_dog.age}")
 my_dog.sit()
 
-print(f"My dog's name is {my_dog.name}")
-print(f"My dog's age is {my_dog.age}")
+print(f"My dog's name is {your_dog.name}")
+print(f"My dog's age is {your_dog.age}")
 your_dog.sit()
+
+
+# Example for understanding classes and instances 
+
+class Car:
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    
+    def get_descriptive_name(self):
+
+        long_name = f"{self.model} {self.make} {self.year}"
+    
+        return long_name.title()
+    
+
+car_name = Car("BMW", "M5", 2022) # Created an instance of class Car
+
+print(car_name.get_descriptive_name())
